3979e1197332266da882f37476ccb50b1ec731b5
Hampus Kjellstrand <hampus.kjellstrand@kungsbacka.se>
2025-03-07 15:25:35 +0100

    Add Definition-file

---
+++ Frends.Kungsbacka.Json/SanitizeCDataSectionsDefinition.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Frends.Kungsbacka.Json
+{
+    /// <summary>
+    /// Result of the SanitizeCDataSections operation.
+    /// </summary>
+    public class SanitizeCDataSectionsResult
+    {
+        /// <summary>
+        /// The sanitized JSON object.
+        /// </summary>
+        public JToken JsonData { get; set; } = JToken.Parse("{}");
+    }
+
+    public class SanitizeCDataSectionsOptions
+    {
+    }
+
+    public class SanitizeCDataSectionsInput
+    {
+        /// <summary>
+        /// The JSON to sanitize.
+        /// </summary>
+        public JToken JsonData { get; set; }
+    }
+}
