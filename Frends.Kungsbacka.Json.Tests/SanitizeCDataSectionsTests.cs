cfc213b6c381683f9a9d6a71207449f937af095a
Hampus Kjellstrand <hampus.kjellstrand@kungsbacka.se>
2025-03-11 11:43:56 +0100

    make tests more readable and do not use 'var'.

--- Frends.Kungsbacka.Json.Tests/SanitizeCDataSectionsTests.cs
+++ Frends.Kungsbacka.Json.Tests/SanitizeCDataSectionsTests.cs
@@ -1,6 +1,8 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Frends.Kungsbacka.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using System;
+using System.Xml.Linq;
 
 namespace Frends.Kungsbacka.Json.Tests
 {
@@ -12,7 +14,7 @@ namespace Frends.Kungsbacka.Json.Tests
         {
             // Arrange
             SanitizeCDataSectionsInput input = null;
-            var options = new SanitizeCDataSectionsOptions();
+            SanitizeCDataSectionsOptions options = new SanitizeCDataSectionsOptions();
 
             // Act
             TestDelegate act = () => JsonTasks.SanitizeCDataSections(input, options);
@@ -25,8 +27,8 @@ namespace Frends.Kungsbacka.Json.Tests
         public void SanitizeCDataSections_ShouldThrowArgumentNullException_WhenJsonDataIsNull()
         {
             // Arrange
-            var input = new SanitizeCDataSectionsInput { JsonData = null };
-            var options = new SanitizeCDataSectionsOptions();
+            SanitizeCDataSectionsInput input = new SanitizeCDataSectionsInput { JsonData = null };
+            SanitizeCDataSectionsOptions options = new SanitizeCDataSectionsOptions();
 
             // Act
             TestDelegate act = () => JsonTasks.SanitizeCDataSections(input, options);
@@ -39,133 +41,176 @@ namespace Frends.Kungsbacka.Json.Tests
         public void SanitizeCDataSections_ShouldReturnEmptyObject_WhenInputIsEmptyJsonObject()
         {
             // Arrange
-            var input = new SanitizeCDataSectionsInput { JsonData = JToken.Parse("{}") };
-            var options = new SanitizeCDataSectionsOptions();
+            SanitizeCDataSectionsInput input = new SanitizeCDataSectionsInput
+            {
+                JsonData = new JObject()
+            };
+            SanitizeCDataSectionsOptions options = new SanitizeCDataSectionsOptions();
 
             // Act
-            var result = JsonTasks.SanitizeCDataSections(input, options);
+            SanitizeCDataSectionsResult result = JsonTasks.SanitizeCDataSections(input, options);
 
             // Assert
-            Assert.AreEqual("{}", result.JsonData.ToString());
+            string expected = "{}";
+            Assert.AreEqual(expected, result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
         }
 
         [Test]
         public void SanitizeCDataSections_ShouldProcessSingleCDataSection()
         {
             // Arrange
-            var input = new SanitizeCDataSectionsInput
+            SanitizeCDataSectionsInput input = new SanitizeCDataSectionsInput
             {
-                JsonData = JToken.Parse("{\"#cdata-section\":\"Some value\"}")
+                JsonData = new JObject { ["#cdata-section"] = "Some value" }
             };
-            var options = new SanitizeCDataSectionsOptions();
+            SanitizeCDataSectionsOptions options = new SanitizeCDataSectionsOptions();
 
             // Act
-            var result = JsonTasks.SanitizeCDataSections(input, options);
+            SanitizeCDataSectionsResult result = JsonTasks.SanitizeCDataSections(input, options);
 
             // Assert
-            Assert.AreEqual("\"Some value\"", result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
+            string expected = "\"Some value\"";
+            Assert.AreEqual(expected, result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
         }
 
         [Test]
         public void SanitizeCDataSections_ShouldProcessNestedCDataSections()
         {
             // Arrange
-            var input = new SanitizeCDataSectionsInput
+            SanitizeCDataSectionsInput input = new SanitizeCDataSectionsInput
             {
-                JsonData = JToken.Parse("{\"parent\":{\"#cdata-section\":\"Nested value\"}}")
+                JsonData = new JObject
+                {
+                    ["parent"] = new JObject
+                    {
+                        ["#cdata-section"] = "Nested value"
+                    }
+                }
             };
-            var options = new SanitizeCDataSectionsOptions();
+            SanitizeCDataSectionsOptions options = new SanitizeCDataSectionsOptions();
 
             // Act
-            var result = JsonTasks.SanitizeCDataSections(input, options);
+            SanitizeCDataSectionsResult result = JsonTasks.SanitizeCDataSections(input, options);
 
             // Assert
-            Assert.AreEqual("{\"parent\":\"Nested value\"}", result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
+            string expected = "{\"parent\":\"Nested value\"}";
+            Assert.AreEqual(expected, result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
         }
 
         [Test]
         public void SanitizeCDataSections_ShouldProcessArraysWithCDataSections()
         {
             // Arrange
-            var input = new SanitizeCDataSectionsInput
+            SanitizeCDataSectionsInput input = new SanitizeCDataSectionsInput
             {
-                JsonData = JToken.Parse("[ {\"#cdata-section\":\"Value1\"}, {\"#cdata-section\":\"Value2\"} ]")
+                JsonData = new JArray
+                {
+                    new JObject { ["#cdata-section"] = "Value1" },
+                    new JObject { ["#cdata-section"] = "Value2" }
+                }
             };
-            var options = new SanitizeCDataSectionsOptions();
+            SanitizeCDataSectionsOptions options = new SanitizeCDataSectionsOptions();
 
             // Act
-            var result = JsonTasks.SanitizeCDataSections(input, options);
+            SanitizeCDataSectionsResult result = JsonTasks.SanitizeCDataSections(input, options);
 
             // Assert
-            Assert.AreEqual("[\"Value1\",\"Value2\"]", result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
+            string expected = "[\"Value1\",\"Value2\"]";
+            Assert.AreEqual(expected, result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
         }
 
         [Test]
         public void SanitizeCDataSections_ShouldLeaveRegularPropertiesUnchanged()
         {
             // Arrange
-            var input = new SanitizeCDataSectionsInput
+            SanitizeCDataSectionsInput input = new SanitizeCDataSectionsInput
             {
-                JsonData = JToken.Parse("{\"name\":\"John\",\"#cdata-section\":\"Some value\"}")
+                JsonData = new JObject
+                {
+                    ["name"] = "John",
+                    ["#cdata-section"] = "Some value"
+                }
             };
-            var options = new SanitizeCDataSectionsOptions();
+            SanitizeCDataSectionsOptions options = new SanitizeCDataSectionsOptions();
 
             // Act
-            var result = JsonTasks.SanitizeCDataSections(input, options);
+            SanitizeCDataSectionsResult result = JsonTasks.SanitizeCDataSections(input, options);
 
             // Assert
-            Assert.AreEqual("{\"name\":\"John\",\"#cdata-section\":\"Some value\"}", result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
+            string expected = "{\"name\":\"John\",\"#cdata-section\":\"Some value\"}";
+            Assert.AreEqual(expected, result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
         }
 
         [Test]
         public void SanitizeCDataSections_ShouldHandleComplexNestedStructure()
         {
             // Arrange
-            var input = new SanitizeCDataSectionsInput
+            SanitizeCDataSectionsInput input = new SanitizeCDataSectionsInput
             {
-                JsonData = JToken.Parse("{\"root\":{\"child\":[{\"#cdata-section\":\"Value1\"},{\"#cdata-section\":\"Value2\"}]}}")
+                JsonData = new JObject
+                {
+                    ["root"] = new JObject
+                    {
+                        ["child"] = new JArray
+                        {
+                            new JObject { ["#cdata-section"] = "Value1" },
+                            new JObject { ["#cdata-section"] = "Value2" }
+                        }
+                    }
+                }
             };
-            var options = new SanitizeCDataSectionsOptions();
+            SanitizeCDataSectionsOptions options = new SanitizeCDataSectionsOptions();
 
             // Act
-            var result = JsonTasks.SanitizeCDataSections(input, options);
+            SanitizeCDataSectionsResult result = JsonTasks.SanitizeCDataSections(input, options);
 
             // Assert
-            Assert.AreEqual("{\"root\":{\"child\":[\"Value1\",\"Value2\"]}}", result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
+            string expected = "{\"root\":{\"child\":[\"Value1\",\"Value2\"]}}";
+            Assert.AreEqual(expected, result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
         }
 
         [Test]
         public void SanitizeCDataSections_ShouldHandleEmptyArray()
         {
             // Arrange
-            var input = new SanitizeCDataSectionsInput
+            SanitizeCDataSectionsInput input = new SanitizeCDataSectionsInput
             {
-                JsonData = JToken.Parse("[]")
+                JsonData = new JArray()
             };
-            var options = new SanitizeCDataSectionsOptions();
+            SanitizeCDataSectionsOptions options = new SanitizeCDataSectionsOptions();
 
             // Act
-            var result = JsonTasks.SanitizeCDataSections(input, options);
+            SanitizeCDataSectionsResult result = JsonTasks.SanitizeCDataSections(input, options);
 
             // Assert
-            Assert.AreEqual("[]", result.JsonData.ToString());
+            string expected = "[]";
+            Assert.AreEqual(expected, result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
         }
 
         [Test]
         public void SanitizeCDataSections_ShouldHandleMixedContent()
         {
             // Arrange
-            var input = new SanitizeCDataSectionsInput
+            SanitizeCDataSectionsInput input = new SanitizeCDataSectionsInput
             {
-                JsonData = JToken.Parse("{\"name\":\"John\",\"data\":{\"#cdata-section\":\"Nested value\"}}")
+                JsonData = new JObject
+                {
+                    ["name"] = "John",
+                    ["data"] = new JObject
+                    {
+                        ["#cdata-section"] = "Nested value"
+                    }
+                }
             };
-            var options = new SanitizeCDataSectionsOptions();
+            SanitizeCDataSectionsOptions options = new SanitizeCDataSectionsOptions();
 
             // Act
-            var result = JsonTasks.SanitizeCDataSections(input, options);
+            SanitizeCDataSectionsResult result = JsonTasks.SanitizeCDataSections(input, options);
 
             // Assert
-            Assert.AreEqual("{\"name\":\"John\",\"data\":\"Nested value\"}", result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
+            string expected = "{\"name\":\"John\",\"data\":\"Nested value\"}";
+            Assert.AreEqual(expected, result.JsonData.ToString(Newtonsoft.Json.Formatting.None));
         }
     }
 }
+
